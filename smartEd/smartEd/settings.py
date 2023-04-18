20c45bb7ded389eb58fc156fff04f2dfee7d8760
HamzaKhalid <Hbaig055@gmail.com>
2023-04-19 03:27:47 +0530

    home page updated

--- smartEd/smartEd/settings.py
+++ smartEd/smartEd/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'accounts',
     'base',
     'course',
-    'reward'
+    'reward',
 ]
 
 MIDDLEWARE = [
--- smartEd/smartEd/urls.py
+++ smartEd/smartEd/urls.py
@@ -1,4 +1,4 @@
-"""smartEd URL Configuration
+"""smartEd RL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -18,8 +18,8 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/', include('base.urls','base')),
-    path('accounts/', include('accounts.urls','accounts')),
-    path('course/', include('course.urls','course')),
-    path('reward/', include('reward.urls','reward')),
+    path('', include('base.urls'),name='base'),
+    path('accounts/', include('accounts.urls'), name='accounts'),
+    path('course/', include('course.urls'), name='course'),
+    path('reward/', include('reward.urls'), name='reward'),
 ]
