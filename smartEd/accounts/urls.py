d91fce9d5014a076101e92f4e0ec5ca7c8b4ff9a
HamzaKhalid <Hbaig055@gmail.com>
2023-04-19 10:17:13 +0530

    student and techer seperated signup

--- smartEd/accounts/urls.py
+++ smartEd/accounts/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path,include
-from .views import SignupView,LoginView,HomeView
+from . import views
 
 app_name = 'accounts'
 
 urlpatterns = [
-    path('signup/',SignupView.as_view(),name='signup'),
-    path('login/',LoginView.as_view(),name='login' ),
-    path('home/',HomeView.as_view(),name='home' ),
+    path('signup-teacher/',views.SignupTeacherView.as_view(),name='signup-teacher'),
+    path('signup-student/',views.SignupStudentView.as_view(),name='signup-student'),
+    path('login/',views.LoginView.as_view(),name='login' ),
+    path('home/',views.HomeView.as_view(),name='home' ),
 
 ]
