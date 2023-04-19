36d21740d4c0a900fa3674915b9507be40bbe9f2
Daniyal Dolare <daniyal.dolare@gmail.com>
2023-04-19 11:16:52 +0530

    models str updated

--- smartEd/reward/models.py
+++ smartEd/reward/models.py
@@ -15,7 +15,7 @@ class RewardRedemption(models.Model):
     date_redeemed = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.student.username} redeemed {self.reward.name} on {self.date_redeemed}"
+        return f"{self.student.user.username} redeemed {self.reward.name} on {self.date_redeemed}"
 
 
 class RewardRequest(models.Model):
@@ -26,7 +26,7 @@ class RewardRequest(models.Model):
     date_rejected = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
-        return f"{self.student.username} requested {self.reward.name}"
+        return f"{self.student.user.username} requested {self.reward.name}"
 
     def is_approved(self):
         return self.date_approved is not None
